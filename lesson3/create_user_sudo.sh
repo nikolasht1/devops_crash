24069cbb4a8ca9394bd1033c90c31465b0212d27
nikolasht1 <nikolasht@outlook.com>
2021-02-24 20:13:42 +0200

    econd commit

---
+++ lesson3/create_user_sudo.sh
@@ -0,0 +1,12 @@
+usermod -aG wheel mykola
+lid -g wheel
+echo 'mykola ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/mykola
+chmod 0440 /etc/sudoers.d/mykola
+chown -R mykola:mykola /home/mykola/.ssh
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g'\ /etc/ssh/sshd_config
+sudo yum update -y
+sudo yum install vsftpd -y
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
+sudo systemctl stop firevalld
+sudo cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.default
